00ab3bd75260ac6609b72d905de29441f48fa85f
Andreas Frömer <andreas.froemer@check24.de>
2021-08-02 21:31:32 +0200

    Add new ConstStrategy
    This strategy will be able to parse consumed constants

---
+++ src/Parser/PHP/Strategy/ConstStrategy.php
@@ -0,0 +1,23 @@
+<?php
+
+declare(strict_types=1);
+
+namespace ComposerUnused\SymbolParser\Parser\PHP\Strategy;
+
+use PhpParser\Node;
+
+final class ConstStrategy implements StrategyInterface
+{
+    public function canHandle(Node $node): bool
+    {
+        return $node instanceof Node\Expr\ConstFetch;
+    }
+
+    /**
+     * @param Node\Expr\ConstFetch $node
+     */
+    public function extractSymbolNames(Node $node): array
+    {
+        return [$node->name->parts[0]];
+    }
+}
