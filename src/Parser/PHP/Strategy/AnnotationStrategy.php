8168f268c625dbf10f7daaed68e58abe14f5cc4f
Leo Viezens <leo.viezens@web.de>
2022-10-02 22:17:07 +0200

    Add AnnotationStrategy

---
+++ src/Parser/PHP/Strategy/AnnotationStrategy.php
@@ -0,0 +1,64 @@
+<?php
+
+declare(strict_types=1);
+
+namespace ComposerUnused\SymbolParser\Parser\PHP\Strategy;
+
+use PhpParser\Node;
+use PhpParser\Node\Expr\ClassConstFetch;
+use PHPStan\PhpDocParser\Ast\PhpDoc\PhpDocChildNode;
+use PHPStan\PhpDocParser\Ast\PhpDoc\PhpDocTagNode;
+use PHPStan\PhpDocParser\Lexer\Lexer;
+use PHPStan\PhpDocParser\Parser\ConstExprParser;
+use PHPStan\PhpDocParser\Parser\PhpDocParser;
+use PHPStan\PhpDocParser\Parser\TokenIterator;
+use PHPStan\PhpDocParser\Parser\TypeParser;
+
+final class AnnotationStrategy implements StrategyInterface
+{
+    private Lexer $lexer;
+    private PhpDocParser $phpDocParser;
+
+    public function __construct(
+        ConstExprParser $constExprParser,
+        Lexer $lexer
+    ) {
+        $this->lexer = $lexer;
+        $typeParser = new TypeParser($constExprParser);
+        $this->phpDocParser = new PhpDocParser($typeParser, $constExprParser);
+    }
+
+    public function canHandle(Node $node): bool
+    {
+        if ($node->getDocComment() === null) {
+            return false;
+        }
+
+        return true;
+    }
+
+    /**
+     * @param Node&ClassConstFetch $node
+     * @return array<string>
+     */
+    public function extractSymbolNames(Node $node): array
+    {
+        $phpDoc = $this->phpDocParser->parse(
+            new TokenIterator($this->lexer->tokenize($node->getDocComment()->getText()))
+        );
+
+        $phpDocTagNodes = array_filter(
+            $phpDoc->children,
+            function (PhpDocChildNode $node): bool {
+                return $node instanceof PhpDocTagNode;
+            }
+        );
+
+        return array_map(
+            function (PhpDocTagNode $n): string {
+                return ltrim($n->name, " \t\n\r\0\x0B@");
+            },
+            $phpDocTagNodes
+        );
+    }
+}
