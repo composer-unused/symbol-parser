7d30395acb96d2710ae314d20b4844e69045d2b8
Laurent Laville <laurent.laville@gmail.com>
2024-02-20 05:23:40 +0000

    add PHP CS Fixer "global_namespace_import" rule to have code homogeneous with global imports

--- .php-cs-fixer.dist.php
+++ .php-cs-fixer.dist.php
@@ -4,6 +4,7 @@ declare(strict_types=1);
 
 return (new PhpCsFixer\Config())
     ->setRules([
+        'global_namespace_import' => ['import_classes' => true, 'import_constants' => true, 'import_functions' => true],
         'no_unused_imports' => true,
     ])
     ->setFinder(
