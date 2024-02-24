e99d85e6449e081bcd2606f74eaee17edcfe873e
Laurent Laville <laurent.laville@gmail.com>
2024-02-24 09:14:28 +0000

    add declare_strict_types if necessary and remove unused import

--- .php-cs-fixer.dist.php
+++ .php-cs-fixer.dist.php
@@ -4,13 +4,16 @@ declare(strict_types=1);
 
 return (new PhpCsFixer\Config())
     ->setRules([
+        'declare_strict_types' => true,
+        'linebreak_after_opening_tag' => true,
+        'blank_line_after_opening_tag' => true,
         'global_namespace_import' => ['import_classes' => true, 'import_constants' => true, 'import_functions' => true],
         'no_unused_imports' => true,
     ])
     ->setFinder(
         PhpCsFixer\Finder::create()
             ->exclude('vendor')
-            ->notPath('assets/TestFiles/UseSingleLineNoGroup.php')
+            ->notPath('assets/')
             ->in([__DIR__.'/src/', __DIR__.'/tests/'])
     )
 ;
--- src/Parser/PHP/Strategy/UsedExtensionSymbolStrategy.php
+++ src/Parser/PHP/Strategy/UsedExtensionSymbolStrategy.php
@@ -1,5 +1,7 @@
 <?php
 
+declare(strict_types=1);
+
 namespace ComposerUnused\SymbolParser\Parser\PHP\Strategy;
 
 use PhpParser\Node;
