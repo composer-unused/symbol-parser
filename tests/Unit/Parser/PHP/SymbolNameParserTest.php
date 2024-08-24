6732a5597cf8ed2f2556ebf75c559c9f1472b1cc
Mihai Stancu <mihai@yarooms.com>
2024-08-25 01:16:53 +0300

    composer-unused/composer-unused#659 Symbol parser test ensuring it skips already visited files

--- tests/Unit/Parser/PHP/SymbolNameParserTest.php
+++ tests/Unit/Parser/PHP/SymbolNameParserTest.php
@@ -4,10 +4,14 @@ declare(strict_types=1);
 
 namespace ComposerUnused\SymbolParser\Test\Unit\Parser\PHP;
 
+use ComposerUnused\SymbolParser\Parser\PHP\DefinedSymbolCollector;
+use ComposerUnused\SymbolParser\Parser\PHP\NameResolver;
 use ComposerUnused\SymbolParser\Parser\PHP\Strategy\ExtendsParseStrategy;
 use ComposerUnused\SymbolParser\Parser\PHP\Strategy\ImplementsParseStrategy;
 use ComposerUnused\SymbolParser\Parser\PHP\Strategy\UseStrategy;
+use ComposerUnused\SymbolParser\Parser\PHP\SymbolNameParser;
 use ComposerUnused\SymbolParser\Test\ParserTestCase;
+use PhpParser\ParserFactory;
 
 final class SymbolNameParserTest extends ParserTestCase
 {
@@ -148,5 +152,45 @@ final class SymbolNameParserTest extends ParserTestCase
         self::assertSame('ezcReflectionMethod', $symbols[1]);
         self::assertSame('ezcReflectionFunction', $symbols[2]);
         self::assertSame('Suit', $symbols[3]);
+    }
+
+
+    /**
+     * @test
+     * @link https://github.com/composer-unused/symbol-parser/issues/136
+     */
+    public function itSkipsAlreadyVisitedFiles(): void
+    {
+        /* @Given a symbol collector which follows includes. */
+        $includes = [];
+        $collector = new DefinedSymbolCollector();
+        $collector->setFileIncludeCallback(function ($file) use (&$includes) {
+            array_push($includes, $file->getPath());
+        });
+        $parser = new SymbolNameParser(
+            (new ParserFactory())->createForNewestSupportedVersion(),
+            new NameResolver(),
+            $collector
+        );
+
+        /* @When the source code includes the same file multiple times. */
+        $code = <<<CODE
+        <?php
+
+        include 'test1.php';
+        include 'test2.php';
+        include 'test3.php';
+        include 'test1.php'; // should get skipped
+        include 'test2.php'; // should get skipped
+        include 'test3.php'; // should get skipped
+        CODE;
+        iterator_to_array($parser->parseSymbolNames($code));
+
+        /* @And the symbol parser (+ collector) does multiple passes on the source code. */
+        iterator_to_array($parser->parseSymbolNames($code));
+
+        /* @Then the collector only followed each unique file once. */
+        self::assertCount(3, $includes);
+        self::assertEquals(['test1.php', 'test2.php', 'test3.php'], $includes);
     }
 }
